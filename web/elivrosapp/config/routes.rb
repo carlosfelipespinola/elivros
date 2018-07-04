27c95984797b9c044b310c49e05fc33d2cacbafc
Carlos Felipe <carlosfelipespiinola@gmail.com>
2018-07-04 19:17:58 -0300

    página cadastro e login configurada

--- web/elivrosapp/config/routes.rb
+++ web/elivrosapp/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root "livros#index"
+  root "application#index"
+  get "cadastro", to: "application#cadastro"
   resources :livros
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
