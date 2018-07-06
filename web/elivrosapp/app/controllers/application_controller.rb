abf4a105aa93355822295b1dd786818ee36bac73
Carlos Felipe <carlosfelipespiinola@gmail.com>
2018-07-06 14:21:36 -0300

    ajustes na lista de livros, renderização da imagem e envio de arquivos pelo servidor

--- web/elivrosapp/app/controllers/application_controller.rb
+++ web/elivrosapp/app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   end
 
   def serve_file
-    send_file(Rails.root.join('public', 'uploads',"#{params[:tipo_arquivo]}/#{params[:nome_arquivo]}.jpg"))
+    send_file(Rails.root.join('public', 'uploads',"#{params[:tipo_arquivo]}/#{params[:nome_arquivo]}.#{params[:format]}"))
   end
 end
